81cf16f6551273b7b7e150e720d04d9212b6c38f
ReeceSpr <42230562+ReeceSpr@users.noreply.github.com>
2023-10-12 16:12:09 +1300

    DB config and retrieval. Loading and errors

---
+++ app/users/page.tsx
@@ -0,0 +1,16 @@
+import { prisma } from "@/lib/prisma";
+import UserCard from "@/components/UserCard/UserCard";
+import styles from './page.module.css'
+
+export default async function Users() {
+    throw new Error('This is an error'); // Error components must be Client components
+    const users = await prisma.user.findMany();
+    
+    return (
+        <div className={styles.grid}>
+            {users.map((user) => {
+                return <UserCard key={user.id} {...user} />
+            })}
+        </div>
+    );
+}
